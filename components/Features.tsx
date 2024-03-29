3476c48b84cb4273ddf870f391a249b467f1bd9a
g4ze <bhaiyajionline17@gmail.com>
2024-03-30 04:41:27 +0530

    landing page

---
+++ components/Features.tsx
@@ -0,0 +1,141 @@
+export default function (){
+    return(<>
+<div className="container my-24 mx-auto md:px-6">
+  <section className="mb-32">
+    <div className="flex flex-wrap items-center">
+      <div className="mb-6 w-full shrink-0 grow-0 basis-auto px-3 lg:mb-0 lg:w-4/12">
+        <h2 className="mb-6 text-3xl font-bold">
+          Why is it so<u className="text-primary dark:text-primary-400">
+            great?</u>
+        </h2>
+
+        <p className="mb-12 text-neutral-500 dark:text-neutral-300">
+          Nunc tincidunt vulputate elit. Mauris varius purus malesuada neque
+          iaculis malesuada. Aenean gravida magna orci, non efficitur est
+          porta id. Donec magna diam.
+        </p>
+      </div>
+
+      <div className="mb-md-0 mb-6 w-full shrink-0 grow-0 basis-auto px-3 lg:w-8/12">
+        <div className="flex flex-wrap">
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 lg:px-3">
+            <div className="flex">
+              <div className="mt-0.5 shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-3 font-bold">Support 24/7</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Pellentesque mollis, metus nec fringilla aliquam. Donec
+                  consequat orci quis volutpat imperdiet.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 lg:px-3">
+            <div className="flex">
+              <div className="mt-0.5 shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-3 font-bold">Tracking</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Magna lacus iaculis elit, quis pharetra varius. Aenean
+                  lectus ex, placerat id tellus in eros.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 lg:px-3">
+            <div className="flex">
+              <div className="mt-0.5 shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-3 font-bold">Reporting</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Pellentesque varius ex vel consequat quis. Sed mauris ex,
+                  imperdiet sit amet nisl ac, ultrices.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 lg:px-3">
+            <div className="flex">
+              <div className="mt-0.5 shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-3 font-bold">Analytics</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Vestibulum gravida iaculis nisl, vel lobortis eros.
+                  Praesent vulputate lacus bibendum augue.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 lg:px-3">
+            <div className="flex">
+              <div className="mt-0.5 shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-3 font-bold">Huge community</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Praesent vulputate lacus bibendum augue. Pellentesque
+                  mollis, metus nec fringilla aliquam.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="mb-12 w-full shrink-0 grow-0 basis-auto md:w-6/12 lg:px-3">
+            <div className="flex">
+              <div className="mt-0.5 shrink-0">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+                  stroke="currentColor" className="mr-3 h-5 w-5 text-primary dark:text-primary-400">
+                  <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+              </div>
+              <div className="ml-2 grow">
+                <p className="mb-3 font-bold">Easy to use</p>
+                <p className="text-neutral-500 dark:text-neutral-300">
+                  Sed mauris ex, imperdiet sit amet nisl ac, ultrices. Magna
+                  lacus iaculis elit, quis pharetra varius.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+</>
+    )
+}
--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,27 +1,26 @@
 import BottomBanner from "./BottomBanner";
+import Features from "./Features";
 
 export default function LandingPage() {
-    return (<>
-        <div className="flex justify-center items-center ">
-            <div className="p-8 rounded-lg">
-                
-                <div className="text-left">
-                    <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-black md:text-5xl lg:text-6xl">
-                       
-                        Safer Earth,<br></br>
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 ">
-                        Lesser Emmissions.
-                        </span> 
-                    </h1>
-                    
-                    <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-                        Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
-                    </p>
+    return (
+        <>
+            <div className="container my-3 mx-auto md:px-2">
+                <div className="p-8 rounded-lg">
+                    <div className="text-left">
+                        <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-black md:text-5xl lg:text-6xl">
+                            Safer Earth,<br></br>
+                            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 ">
+                                Lesser Emissions.
+                            </span> 
+                        </h1>
+                        <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+                            Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.
+                        </p>
+                    </div>
                 </div>
             </div>
-            
-        </div>
-        <BottomBanner />
+            <div className="hidden md:block"><Features /></div>
+            <BottomBanner />
         </>
     );
 }
